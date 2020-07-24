63e25853b8922e83014e44c2b8f135e638e3fc27
vaibhavbahel <55796901+vaibhavbahel@users.noreply.github.com>
2020-07-24 14:43:47 +0530

    Add files via upload

---
+++ Data_management/dir_management.py
@@ -0,0 +1,16 @@
+import os
+import glob
+from shutil import copyfile
+
+os.chdir('/home/vaibhav/tejas research/data/') #where all the separate folders are
+main_dir = os.listdir()
+i=1
+for x in range(1,len(main_dir)+1):
+    for folder in main_dir:
+        os.chdir('/home/vaibhav/tejas research/data/'+folder+'/')
+        files = glob.glob("*.jpg")
+        for z in files:
+            copyfile(z,'/home/vaibhav/tejas research/images/image_'+str(i)+'.jpg')
+            i=i+1
+            
+print(i)        
